18e3d71c16fa427bdb7e343eb41f8c88ab587b7e
edrapac <emerson.drapac@mnsu.edu>
2019-03-15 13:09:07 -0500

    fixed all that shitty formatting, feelsgoodman.jpeg

--- Node.py
+++ Node.py
@@ -1,21 +1,11 @@
-# created seperate node file for portability
 class Node:
 
-	def __init__(self, content, next_node = None):
-		self.content = content
-		self.next_node = next_node
+    def __init__(self, content, next_node=None, name=None):
+        self.content = content
+        self.next_node = next_node
 
-	def setContent(self,content):
-		self.content = content
+        self.name = name
 
-	def getContent(self):
-		return self.content
-
-	def setNext(self,next_node):
-		self.next_node = next_node
-
-	def getNext(self):
-		return self.next_node
-
-	def __repr__(self):       #this is mainly for ease of use so we can just print a node and get its content without having to explicitly call methods
-		return self.content
+    def __repr__(self):  # this is mainly for ease of use so we can just print a node and get its
+        # content without having to explicitly call methods
+        return self.name
